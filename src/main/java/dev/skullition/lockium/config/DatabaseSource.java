db475e5a56038d8491fcc2583bc032f107ddb03c
Skullition <78258797+Skullition@users.noreply.github.com>
2024-07-21 01:17:20 +0700

    Add DatabaseSource and update application.properties with hikari & flyway values

---
+++ src/main/java/dev/skullition/lockium/config/DatabaseSource.java
@@ -0,0 +1,21 @@
+package dev.skullition.lockium.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+import io.github.freya022.botcommands.api.core.db.HikariSourceSupplier;
+import io.github.freya022.botcommands.api.core.service.annotations.BService;
+import org.jetbrains.annotations.NotNull;
+
+@BService
+public class DatabaseSource implements HikariSourceSupplier {
+    private final HikariDataSource dataSource;
+
+    public DatabaseSource(HikariDataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @NotNull
+    @Override
+    public HikariDataSource getSource() {
+        return dataSource;
+    }
+}

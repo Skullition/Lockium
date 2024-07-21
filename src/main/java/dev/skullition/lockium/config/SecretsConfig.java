15448aabc23c953de8b27fd16776d52f51fc8a3b
Skullition <78258797+Skullition@users.noreply.github.com>
2024-07-21 22:56:43 +0700

    Add list of additional spring configs for botcommands framework

--- src/main/java/dev/skullition/lockium/config/SecretsConfig.java
+++ src/main/java/dev/skullition/lockium/config/SecretsConfig.java
@@ -2,8 +2,6 @@ package dev.skullition.lockium.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-import java.util.List;
-
 @ConfigurationProperties("secrets")
-public record SecretsConfig(String token, List<String> ownerIds) {
+public record SecretsConfig(String token) {
 }

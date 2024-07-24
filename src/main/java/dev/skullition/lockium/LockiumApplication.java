5d9ae2ac20a0a87aaede08bb9e60e98c52acd2f0
Skullition <78258797+Skullition@users.noreply.github.com>
2024-07-24 15:17:39 +0700

    Update BotCommands to autoconfiguration beta

--- src/main/java/dev/skullition/lockium/LockiumApplication.java
+++ src/main/java/dev/skullition/lockium/LockiumApplication.java
@@ -9,7 +9,6 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 
 @EnableLockiumBot
 @SpringBootApplication
-@EnableBotCommands
 @EnableConfigurationProperties(SecretsConfig.class)
 public class LockiumApplication {
 

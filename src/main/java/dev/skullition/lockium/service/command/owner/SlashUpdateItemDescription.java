991139beffa4880e51c4293892b39cc7e85e172f
Skullition <78258797+Skullition@users.noreply.github.com>
2024-10-05 21:44:08 +0700

    Add logic for onSlashUpdate to get item description from wiki

--- src/main/java/dev/skullition/lockium/service/command/owner/SlashUpdateItemDescription.java
+++ src/main/java/dev/skullition/lockium/service/command/owner/SlashUpdateItemDescription.java
@@ -1,5 +1,6 @@
 package dev.skullition.lockium.service.command.owner;
 
+import dev.skullition.lockium.model.GrowtopiaItem;
 import dev.skullition.lockium.proxy.GrowtopiaWikiProxy;
 import io.github.freya022.botcommands.api.commands.annotations.Command;
 import io.github.freya022.botcommands.api.commands.application.ApplicationCommand;
@@ -7,14 +8,19 @@ import io.github.freya022.botcommands.api.commands.application.slash.GuildSlashE
 import io.github.freya022.botcommands.api.commands.application.slash.annotations.JDASlashCommand;
 import io.github.freya022.botcommands.api.commands.application.slash.annotations.SlashOption;
 import io.github.freya022.botcommands.api.commands.application.slash.annotations.TopLevelSlashCommandData;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import net.dv8tion.jda.api.entities.Message;
 import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-/**
-* Command to update item description based on attached file.
-*/
+/** Command to update item description based on attached file. */
 @Command
 public class SlashUpdateItemDescription extends ApplicationCommand {
+  private static final Logger logger = LoggerFactory.getLogger(SlashUpdateItemDescription.class);
   private final GrowtopiaWikiProxy proxy;
 
   public SlashUpdateItemDescription(GrowtopiaWikiProxy proxy) {
@@ -30,6 +36,31 @@ public class SlashUpdateItemDescription extends ApplicationCommand {
       GuildSlashEvent event,
       @NotNull @SlashOption(name = "file", description = "File to update from")
           Message.Attachment file) {
-    event.reply(file.getFileName()).queue();
+    file.getProxy().download().thenAccept(action -> consumeFile(action, event));
+    event.reply("Updating the item descriptions from " + file.getFileName()).queue();
+  }
+
+  private void consumeFile(InputStream stream, GuildSlashEvent event) {
+    try (BufferedReader reader = new BufferedReader(new InputStreamReader(stream))) {
+      for (String line = reader.readLine(); line != null; line = reader.readLine()) {
+        String[] split = line.split("\\|");
+        
+        String itemId = split[0];
+        String itemName = split[1];
+        logger.debug("Getting item description of {} ({})", itemName, itemId);
+        var result = proxy.getItemData(itemName);
+        if (result.isEmpty()) {
+          event.getHook().editOriginal("Couldn't get wiki entry for " + itemName).queue();
+          return;
+        }
+        
+        GrowtopiaItem item = result.get();
+        // TODO: Process obtained item desc
+        logger.info("{} | {}", itemName, item.description());
+      }
+      stream.close();
+    } catch (IOException e) {
+      logger.error("Couldn't read item description file", e);
+    }
   }
 }

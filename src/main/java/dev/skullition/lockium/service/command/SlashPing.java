109ccbd9538a2f54c7a3845f9169984ccceb0bc9
Skullition <78258797+Skullition@users.noreply.github.com>
2024-10-07 11:41:37 +0700

    Document SlashPing

--- src/main/java/dev/skullition/lockium/service/command/SlashPing.java
+++ src/main/java/dev/skullition/lockium/service/command/SlashPing.java
@@ -5,8 +5,14 @@ import io.github.freya022.botcommands.api.commands.application.ApplicationComman
 import io.github.freya022.botcommands.api.commands.application.slash.GuildSlashEvent;
 import io.github.freya022.botcommands.api.commands.application.slash.annotations.JDASlashCommand;
 
+/** Command to get current REST ping. */
 @Command
 public class SlashPing extends ApplicationCommand {
+  /**
+   * Handles the {@code /ping } command. Sends a response with the current ping.
+   *
+   * @param event the {@link GuildSlashEvent} representing the slash command interaction
+   */
   @JDASlashCommand(name = "ping", description = "Gets the REST ping.")
   public void onSlashPing(GuildSlashEvent event) {
     event.deferReply(true).queue();

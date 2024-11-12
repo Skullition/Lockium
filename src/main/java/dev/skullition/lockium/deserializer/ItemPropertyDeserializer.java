6b4bbf051081298fd3ef07562c807a82c88ae485
Skullition <78258797+Skullition@users.noreply.github.com>
2024-11-12 18:53:47 +0700

    Add item property parsing from int to enum for GrowtopiaItem

---
+++ src/main/java/dev/skullition/lockium/deserializer/ItemPropertyDeserializer.java
@@ -0,0 +1,74 @@
+package dev.skullition.lockium.deserializer;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonNode;
+import dev.skullition.lockium.model.GrowtopiaItem;
+import java.io.IOException;
+import java.util.Set;
+
+/**
+ * Deserializes an int property flag for items to a {@link Set} of {@link
+ * dev.skullition.lockium.model.GrowtopiaItem.ItemProperty}.
+ */
+public class ItemPropertyDeserializer extends JsonDeserializer<Set<GrowtopiaItem.ItemProperty>> {
+  /**
+   * Method that can be called to ask implementation to deserialize JSON content into the value type
+   * this serializer handles. Returned instance is to be constructed by method itself.
+   *
+   * <p>Pre-condition for this method is that the parser points to the first event that is part of
+   * value to deserializer (and which is never JSON 'null' literal, more on this below): for simple
+   * types it may be the only value; and for structured types the Object start marker or a
+   * FIELD_NAME.
+   *
+   * <p>The two possible input conditions for structured types result from polymorphism via fields.
+   * In the ordinary case, Jackson calls this method when it has encountered an OBJECT_START, and
+   * the method implementation must advance to the next token to see the first field name. If the
+   * application configures polymorphism via a field, then the object looks like the following.
+   *
+   * <pre>
+   *      {
+   *          "@class": "class name",
+   *          ...
+   *      }
+   *  </pre>
+   * <p></p>
+   * Jackson consumes the two tokens (the <tt>@class</tt> field name and its value) in order to
+   * learn the class and select the deserializer. Thus, the stream is pointing to the FIELD_NAME for
+   * the first field after the @class. Thus, if you want your method to work correctly both with and
+   * without polymorphism, you must begin your method with:
+   *
+   * <pre>
+   *       if (p.currentToken() == JsonToken.START_OBJECT) {
+   *         p.nextToken();
+   *       }
+   *  </pre>
+   * <p></p>
+   * This results in the stream pointing to the field name, so that the two conditions align.
+   *
+   * <p>Post-condition is that the parser will point to the last event that is part of deserialized
+   * value (or in case deserialization fails, event that was not recognized or usable, which may be
+   * the same event as the one it pointed to upon call).
+   *
+   * <p><strong>Handling null values (JsonToken.VALUE_NULL)</strong> <br>
+   * Note that this method is never called for the JSON {@code null} literal to avoid every
+   * deserializer from having to handle null values. Instead, the {@link
+   * JsonDeserializer#getNullValue(DeserializationContext)} method is called to produce a null
+   * value. To influence null handling, custom deserializers should override {@link
+   * JsonDeserializer#getNullValue(DeserializationContext)} and usually also {@link
+   * JsonDeserializer#getNullAccessPattern()}.
+   *
+   * @param parser Parser used for reading JSON content
+   * @param context Context that can be used to access information about this deserialization
+   *     activity.
+   * @return Deserialized value
+   */
+  @Override
+  public Set<GrowtopiaItem.ItemProperty> deserialize(
+      JsonParser parser, DeserializationContext context) throws IOException {
+    JsonNode node = parser.getCodec().readTree(parser);
+    int flags = node.asInt();
+    return GrowtopiaItem.ItemProperty.fromInt(flags);
+  }
+}
--- src/main/java/dev/skullition/lockium/model/GrowtopiaItem.java
+++ src/main/java/dev/skullition/lockium/model/GrowtopiaItem.java
@@ -1,13 +1,43 @@
 package dev.skullition.lockium.model;
 
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import dev.skullition.lockium.deserializer.ItemPropertyDeserializer;
+import java.util.Arrays;
+import java.util.EnumSet;
+import java.util.stream.Collectors;
 
 public record GrowtopiaItem(
-    @NotNull String spriteUrl,
-    @NotNull String itemWikiUrl,
-    int rarity,
-    @NotNull String description,
-    @NotNull String properties,
-    @NotNull GrowtopiaItemField itemField,
-    @Nullable ItemEffects itemEffects) {}
+    int id,
+    String description,
+    @JsonDeserialize(using = ItemPropertyDeserializer.class) EnumSet<ItemProperty> properties) {
+  public enum ItemProperty {
+    MULTI_FACING(0x01),
+    WRENCH_ABLE(0x02),
+    NO_SEED(0x04),
+    PERMANENT(0x08),
+    NO_DROP(0x10),
+    NO_SELF(0x20),
+    NO_SHADOW(0x40),
+    WORLD_LOCK(0x80),
+    BETA(0x100),
+    AUTO_PICKUP(0x200),
+    MOD(0x400),
+    RANDOM_GROW(0x800),
+    PUBLIC(0x1000),
+    FOREGROUND(0x2000),
+    HOLIDAY(0x4000),
+    UNTRADABLE(0x8000);
+
+    private final int value;
+
+    ItemProperty(int flag) {
+      this.value = flag;
+    }
+
+    public static EnumSet<ItemProperty> fromInt(int flags) {
+      return Arrays.stream(values())
+          .filter(property -> (flags & property.value) != 0)
+          .collect(Collectors.toCollection(() -> EnumSet.noneOf(ItemProperty.class)));
+    }
+  }
+}

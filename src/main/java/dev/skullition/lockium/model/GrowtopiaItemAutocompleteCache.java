9fb16d7b83f9fa1ff94bdcb1bc3509046dd41895
Skullition <78258797+Skullition@users.noreply.github.com>
2024-11-19 21:21:33 +0700

    Add isTradable and isSeed parameters for GrowtopiaItemAutocompleteCache DTO

--- src/main/java/dev/skullition/lockium/model/GrowtopiaItemAutocompleteCache.java
+++ src/main/java/dev/skullition/lockium/model/GrowtopiaItemAutocompleteCache.java
@@ -7,5 +7,8 @@ import org.jetbrains.annotations.NotNull;
  *
  * @param name the Growtopia item name
  * @param id the id corresponding to the item
+ * @param isTradable whether this item can be traded among players.
+ * @param isSeed whether this item is a seed.
  */
-public record GrowtopiaItemAutocompleteCache(@NotNull String name, int id) {}
+public record GrowtopiaItemAutocompleteCache(
+    @NotNull String name, int id, boolean isTradable, boolean isSeed) {}

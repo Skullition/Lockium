215e97d8eb9f386a2a0b5768e20baf71261f0aa2
sskulli <78258797+Skullition@users.noreply.github.com>
2025-01-14 19:01:16 +0000

    Update flyway script

--- src/main/resources/db/migration/lockium/V1.0.0.2025.01.05__Create_database.sql
+++ src/main/resources/db/migration/lockium/V1.0.0.2025.01.05__Create_database.sql
@@ -1,4 +1,7 @@
-CREATE TABLE lockium.allowed_self_role
+create schema lockium;
+set schema 'lockium';
+
+CREATE TABLE allowed_self_role
 (
     role_id  BIGINT NOT NULL,
     name     VARCHAR(255),
@@ -6,7 +9,7 @@ CREATE TABLE lockium.allowed_self_role
     CONSTRAINT pk_allowed_self_role PRIMARY KEY (role_id)
 );
 
-CREATE TABLE lockium.self_role
+CREATE TABLE self_role
 (
     role_id BIGINT NOT NULL,
     name    TEXT   NOT NULL,
